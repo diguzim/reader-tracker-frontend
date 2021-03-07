a355d48b623a9cdc57b869d03395f52ba34bd5d6
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2021-03-07 18:38:48 -0300

    Adding styled components to project, creating initial Button component

---
+++ components/Button/Button.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface StyledButtonProps {
+  backgroundColor: string
+  color: string
+  fontSize: string
+  padding: string
+  boxShadow: string
+  onClick?: () => void
+}
+
+export interface ButtonProps extends Partial<StyledButtonProps> {
+  primary?: boolean
+  size?: 'small' | 'medium' | 'large'
+  label: string
+}
+
+const StyledButton = styled.button<StyledButtonProps>((props) => ({
+  fontFamily: 'Nunito Sans, Helvetica Neue, Helvetica, Arial, sans-serif',
+  fontWeight: 700,
+  border: 0,
+  borderRadius: '3em',
+  cursor: 'pointer',
+  display: 'inline-block',
+  lineHeight: 1,
+  backgroundColor: props.backgroundColor,
+  color: props.color,
+  fontSize: props.fontSize,
+  padding: props.padding,
+  boxShadow: props.boxShadow,
+}))
+
+export const Button: React.FC<ButtonProps> = ({
+  primary = false,
+  size = 'medium',
+  fontSize: incomingFontSize,
+  backgroundColor: incomingBackgroundColor,
+  color: incomingColor,
+  padding: incomingPadding,
+  label,
+  boxShadow: incomingBoxShadow,
+  ...props
+}) => {
+  const backgroundColor = incomingBackgroundColor
+    ? incomingBackgroundColor
+    : primary
+    ? '#1ea7fd'
+    : 'transparent'
+  const color = incomingColor ? incomingColor : primary ? 'white' : '#333'
+  const fontSize = incomingFontSize
+    ? incomingFontSize
+    : size === 'small'
+    ? '12px'
+    : size === 'medium'
+    ? '14px'
+    : '16px'
+  const padding = incomingPadding
+    ? incomingPadding
+    : size === 'small'
+    ? '10px 16px'
+    : size === 'medium'
+    ? '11px 20px'
+    : '12px 24px'
+  const boxShadow = incomingBoxShadow
+    ? incomingBoxShadow
+    : primary
+    ? 'none'
+    : 'rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset'
+  return (
+    <StyledButton
+      type="button"
+      backgroundColor={backgroundColor}
+      color={color}
+      fontSize={fontSize}
+      padding={padding}
+      boxShadow={boxShadow}
+      {...props}
+    >
+      {label}
+    </StyledButton>
+  )
+}
